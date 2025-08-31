021bb2306a9ef16bf27f5d0d5072135066626baf
kdiggety <wakes-pillory.01@icloud.com>
2025-08-31 14:20:01 -0700

    trying to fix the build tests

--- backend/__tests__/health.test.js
+++ backend/__tests__/health.test.js
@@ -1,6 +1,6 @@
-import request from "supertest";
+const request = require('supertest');
 const express = require('express');
-import routes from "../src/routes.js";
+const routes = require('../src/routes');  // drop `.js` if in CJS
 
 test("health", async () => {
   const app = express();
