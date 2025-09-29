f43e60ff8d24a9b3f1075b0c63f9774561e1b4f8
kdiggety <wakes-pillory.01@icloud.com>
2025-09-28 21:21:56 -0700

    troubleshooting frontend oidc

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,6 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 app.use("/api", routes);
-app.use("/api/auth", authRouter);
 
 // Prometheus metrics
 const register = new client.Registry();
